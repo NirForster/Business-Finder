f4f9a8603b78800381dffecf8b69407620b286ab
NirForster <162992599+NirForster@users.noreply.github.com>
2024-12-30 12:10:45 +0200

    alkdjf

--- Server/app.js
+++ Server/app.js
@@ -31,9 +31,9 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-app.use((req, res, next) => {
-  res.status(404).json({ error: "Route not found" });
-});
+// app.use((req, res, next) => {
+//   res.status(404).json({ error: "Route not found" });
+// });
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
